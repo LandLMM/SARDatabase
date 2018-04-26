1d62707cb6f3f32899b9e119af5cc6c1936742b6
landlm <marcin.chudy.office@gmail.com>
2018-04-26 20:06:55 +0200

    This is the first commit which begins the SARDatabase Project.

---
+++ src/test/java/pl/sdacademy/sardatabase/SardatabaseApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sdacademy.sardatabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SardatabaseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
